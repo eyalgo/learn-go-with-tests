84d5ef32ef6eae2fb30f71ca3a03f32939c65dac
Eyal Golan <eral.golan@spark.net>
2019-09-10 14:56:14 +0200

    Add Spanish and French support

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+const SPANISH = "Spanish"
+const FRENCH = "French"
 const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Bonjour, "
+const spanishHelloPrefix = "Hola, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
         name = "World"
-    }
-	return englishHelloPrefix + name
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case FRENCH:
+		prefix = frenchHelloPrefix
+	case SPANISH:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
