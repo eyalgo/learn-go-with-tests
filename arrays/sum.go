976990fa6d7dd4ff074592add5670160fbd10442
Eyal Golan <eral.golan@spark.net>
2019-09-10 17:13:05 +0200

    Using private method for the tail to avoid duplicate code

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,9 +1,30 @@
 package main
 
-func Sum(numbers []int) int {
+func _sum(numbers []int, tailIndex int) int {
+	tail := numbers[tailIndex:]
 	sum := 0
-	for _, number := range numbers {
+	for _, number := range tail {
 		sum += number
 	}
 	return sum
+}
+
+func Sum(numbers []int) int {
+	return _sum(numbers, 0)
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, _sum(numbers, 0))
+	}
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, _sum(numbers, 1))
+	}
+	return sums
 }
