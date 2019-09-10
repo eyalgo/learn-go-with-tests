f2065124ac1c6f82295fb9c527de2a31cf94305f
Eyal Golan <eral.golan@spark.net>
2019-09-10 17:27:39 +0200

    Support empty slice. Refactor cleaner tests.

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -5,6 +5,13 @@ import (
 	"testing"
 )
 
+func _checkSums(t *testing.T, got, want []int) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
 		numbers := []int{1, 2, 3}
@@ -19,20 +26,29 @@ func TestSum(t *testing.T) {
 }
 
 func TestSumAll(t *testing.T) {
+	t.Run("no tail - make the sums of some slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+		_checkSums(t, got, want)
+	})
 
-	got := SumAll([]int{1,2}, []int{0,9})
-	want := []int{3, 9}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	t.Run("no tail - safely sum empty slices", func(t *testing.T) {
+		got := SumAll([]int{}, []int{3, 4, 5})
+		want := []int{0, 12}
+		_checkSums(t, got, want)
+	})
 }
 
 func TestSumAllTails(t *testing.T) {
-	got := SumAllTails([]int{1,2}, []int{0,9})
-	want := []int{2, 9}
+	t.Run("with tail - make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		_checkSums(t, got, want)
+	})
 
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %v want %v", got, want)
-	}
+	t.Run("with tail - safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
+		_checkSums(t, got, want)
+	})
 }
