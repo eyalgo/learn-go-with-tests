099ed9d1eaac2c2a3b93c4e96f929871f1d75b72
Eyal Golan <eral.golan@spark.net>
2019-09-11 12:21:21 +0200

    Add Perimeter to Shape. Refactor the test to use Table Driven Tests

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,9 +1,10 @@
-package structs
+package shapes
 
-import math "math"
+import "math"
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -15,6 +16,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,6 +28,20 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-func Perimeter(rectangle Rectangle) float64 {
-	return 2 * (rectangle.Width + rectangle.Height)
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
+}
+
+func (t Triangle) Perimeter() float64 {
+	cEdge := math.Sqrt(math.Pow(t.Height, 2) + math.Pow(t.Base/2, 2))
+	return cEdge*2 + t.Base
 }
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -1,36 +1,32 @@
-package structs
+package shapes
 
 import "testing"
 
-func _assertCorrectMessage(t *testing.T, got, want float64) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+var tests = []struct {
+	name         string
+	shape        Shape
+	hasPerimeter float64
+	hasArea      float64
+}{
+	{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasPerimeter: 36.0, hasArea: 72.0},
+	{name: "Circle", shape: Circle{Radius: 10}, hasPerimeter: 62.83185307179586, hasArea: 314.1592653589793},
+	{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasPerimeter: 28.97056274847714, hasArea: 36.0},
 }
 
 func TestPerimeter(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
-	want := 40.0
-	_assertCorrectMessage(t, got, want)
+	for _, tt := range tests {
+		got := tt.shape.Perimeter()
+		if got != tt.hasPerimeter {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasPerimeter)
+		}
+	}
 }
 
 func TestArea(t *testing.T) {
-	_checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %.g", got, want)
+	for _, tt := range tests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
 	}
-	t.Run("Rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12, 6}
-		_checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("Circles", func(t *testing.T) {
-		circle := Circle{10}
-		_checkArea(t, circle, 314.1592653589793)
-	})
 }
